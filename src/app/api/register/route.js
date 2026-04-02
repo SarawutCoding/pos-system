ee68679c464af46214803182946e3f1ad4822261
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-02 22:08:45 +0700

    save

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -3,6 +3,7 @@ import User from "../../../../model/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
+
 export async function POST(res) {
     const { userName, email, password } = await res.json();
     const hashPassword = await bcrypt.hash(password, 10);
