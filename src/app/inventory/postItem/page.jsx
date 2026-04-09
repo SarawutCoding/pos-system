8119eb59578c1fc73f13438c574021c200bdb357
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-09 21:49:41 +0700

    save

--- src/app/inventory/postItem/page.jsx
+++ src/app/inventory/postItem/page.jsx
@@ -6,8 +6,15 @@ const PostItemPasge = () => {
   const [productName, setProductName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState(0);
-  const [quantity, seyQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(0);
   const [fileImge, setFileImge] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(productName, description, price, quantity, fileImge);
+    
+
+  }
   return (
     <div className="min-h-[calc(100vh-80px)] bg-gray-50 flex items-center justify-center p-6">
       <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md border border-gray-100">
@@ -18,12 +25,12 @@ const PostItemPasge = () => {
         </div>
 
         {/* Form */}
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">ProductName</label>
             <input 
               type="text"
-              onChange={(e) => {setUserName(e.target.value)}}
+              onChange={(e) => {setProductName(e.target.value)}}
               placeholder="ชื่อสินค้า"
               className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:ring-2 focus:ring-amber-400 focus:border-transparent outline-none transition-all"
             />
@@ -33,7 +40,7 @@ const PostItemPasge = () => {
             <label className="block text-sm font-medium text-gray-700 mb-1">Description</label>
             <textarea 
               className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:ring-2 focus:ring-amber-400 focus:border-transparent outline-none transition-all"
-              onChange={(e) => {setEmail(e.target.value)}}
+              onChange={(e) => {setDescription(e.target.value)}}
               placeholder='รายละเอียดสินค้า'></textarea>
           </div>
 
@@ -41,7 +48,7 @@ const PostItemPasge = () => {
             <label className="block text-sm font-medium text-gray-700 mb-1">Price</label>
             <input 
               type="number"
-              onChange={(e) => {setPassword(e.target.value)}}
+              onChange={(e) => {setPrice(e.target.value)}}
               placeholder="ราคา"
               className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:ring-2 focus:ring-amber-400 focus:border-transparent outline-none transition-all"
             />
@@ -52,7 +59,7 @@ const PostItemPasge = () => {
             <label className="block text-sm font-medium text-gray-700 mb-1">Quantity</label>
             <input 
               type="number"
-              onChange={(e) => {setConfirmPassword(e.target.value)}}
+              onChange={(e) => {setQuantity(e.target.value)}}
               placeholder="จำนวน"
               className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:ring-2 focus:ring-amber-400 focus:border-transparent outline-none transition-all"
             />
@@ -62,7 +69,7 @@ const PostItemPasge = () => {
             <label className="block text-sm font-medium text-gray-700 mb-1">Imge</label>
             <input 
               type="file"
-              onChange={(e) => {setConfirmPassword(e.target.value)}}
+              onChange={(e) => {setFileImge(e.target.files[0])}}
               className="w-full text-sm text-sky-600 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:bg-sky-100 file:text-sky-700 hover:file:bg-sky-200 cursor-pointer"
             />
           </div>
