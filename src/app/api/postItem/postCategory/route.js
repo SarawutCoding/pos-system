a1d5ccf1a1da5e19ece190c66ac1d4e7a639f9d0
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-12 22:47:06 +0700

    save

--- src/app/api/postItem/postCategory/route.js
+++ src/app/api/postItem/postCategory/route.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
+import { connectMongo } from "../../../../../lib/mongoConnect";
+import Category from "../../../../../model/category";
 
 export async function POST(res) {
     const { categoryName } = await res.json();
-    console.log(categoryName);
+    connectMongo();
+    await Category.create({ name: categoryName })
     return NextResponse.json({ message: "OK" }, { status: 200 })
 }
