a43a9a228b16ed6f31db3150900e2c48a2b70688
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-19 21:10:36 +0700

    save

---
+++ src/app/components/categoty/InsertCategory.jsx
@@ -0,0 +1,12 @@
+import { connectMongo } from "../../../../lib/mongoConnect";
+import Category from "../../../../model/category";
+import PostCategory from "./PostCategory";
+import React from 'react'
+
+const InsertCategory = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default InsertCategory
---
+++ src/app/components/categoty/PostCategory.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const PostCategory = () => {
+  return (
+    <div>PostCategory</div>
+  )
+}
+
+export default PostCategory
