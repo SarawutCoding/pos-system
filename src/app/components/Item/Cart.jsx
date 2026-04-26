4ea46336b8ca4c0c02377defe3504ed13d302bf5
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-26 20:33:51 +0700

    save

--- src/app/components/Item/Cart.jsx
+++ src/app/components/Item/Cart.jsx
@@ -2,19 +2,10 @@
 import React, { useEffect } from 'react'
 
 const Cart = ({ listCart }) => {
-    const ListData = [];
-    ListData.push(listCart);
-    console.log(ListData);
     
   return (
     <div>
-        {ListData && ListData.length > 0 ? (
-            ListData.map(val => (
-                <div key={val._id}>{val.name}</div>
-            ))
-        ) : (
-            <div>ไม่มีข้อมูล</div>
-        )}
+        
     </div>
   )
 }
