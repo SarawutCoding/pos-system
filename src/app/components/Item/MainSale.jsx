0bdb07f5154d9a18efa13652720186697e1bbffd
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-29 23:26:11 +0700

    saveAll

--- src/app/components/Item/MainSale.jsx
+++ src/app/components/Item/MainSale.jsx
@@ -43,13 +43,12 @@ const MainSale = ({ products, category }) => {
         )}
       </ul>
       
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-3 gap-4 p-5 overflow-y-auto w-3/4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-2 gap-4 p-5 overflow-y-auto w-3/4">
         {checkProduct?.map((item) => (
           <ProductCard key={item._id} product={item} />
         ))}
-        <Cart/>
       </div>
-      
+      <Cart/>
     </main>
   );
 };
